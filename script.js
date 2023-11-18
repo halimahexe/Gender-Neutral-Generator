1930a444e28c1723722ede516f8cb4c2927179b6
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-18 21:43:48 +0000

    adding more terms and css styling

--- script.js
+++ script.js
@@ -5,13 +5,67 @@ let btn = document.getElementById('new-term');
 
 let terms = [{
     term: 'Sibling',
-    desc: 'Alternative to sister/brother'
+    desc: 'Sister/brother'
 }, {
     term: 'Nibling',
-    desc: 'Your sibling\'s child - alternative to niece/nephew'
+    desc: 'Niece/nephew'
 }, {
     term: 'Spouse',
-    desc: 'Married partner - alternative to husband/wife',
+    desc: 'Husband/wife',
+}, {
+    term: 'Significant other',
+    desc: 'Boyfriend/girlfriend'
+}, {
+    term: 'Partner',
+    desc: 'Husband/wife/boyfriend/girlfriend'
+}, {
+    term: 'Friend',
+    desc: 'Dude/buddy'
+}, {
+    term: 'Betrothed',
+    desc: 'Fiancé(e)'
+}, {
+    term: 'Grandchild',
+    desc: 'Granddaughter/grandson'
+}, {
+    term: 'Grandparent',
+    desc: 'Grandma/grandad'
+}, {
+    term: 'Child',
+    desc: 'Son/daughter'
+}, {
+    term: 'Business person',
+    desc: 'Businessman/woman'
+}, {
+    term: 'Chairperson',
+    desc: 'Chairman/woman'
+}, {
+    term: 'Foreperson',
+    desc: 'Foreman'
+}, {
+    term: 'Layperson',
+    desc: 'Layman'
+}, {
+    term: 'Postal worker/Postie',
+    desc: 'Postman'
+}, {
+    term: 'Crewed',
+    desc: 'Manned'
+}, {
+    term: 'Mx',
+    desc: 'Mr/Mrs'
+}, {
+    term: 'Server',
+    desc: 'Waiter/waitress'
+}, {
+    term: 'Young person',
+    desc: 'Girl/boy'
+}, {
+    term: 'Adult',
+    desc: 'Man/woman'
+}, {
+    term: 'They',
+    desc: 'He/she'
 }]
 
 btn.addEventListener('click', function() {
